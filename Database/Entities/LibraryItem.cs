ad8cf45f5b7d73795cfca78114506caf28d97512
Michał Kamiński <michal.kaminski@neosystem.pl>
2019-05-12 13:42:46 +0200

    Adding 2 Projects Auth and Databse

---
+++ Database/Entities/LibraryItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Database.Entities
+{
+    public enum LibraryItemType { DVD, CD, Book };
+
+    class LibraryItem
+    {
+        public int Id { get; set; }
+
+        public string Author { get; set; }
+
+        public LibraryItemType ItemType { get; set; }
+
+        public string Cover { get; set; }
+
+        public DateTime IssueYear { get; set; }
+
+        public int OwnerId { get; set; }
+
+
+
+        public bool IsDone { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public DateTimeOffset? DueAt { get; set; }
+    }
+}
