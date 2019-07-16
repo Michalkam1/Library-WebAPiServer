fd8edf1572c8d9b1b5ac04b04dc2fa88c562ffcc
Matti <mateusz.szerszen@hotmail.com>
2019-07-16 10:48:16 +0200

    Zmiany w formularzu, poprawki błędów.

--- ClientWebApp/Controllers/LibraryItemController.cs
+++ ClientWebApp/Controllers/LibraryItemController.cs
@@ -11,21 +11,20 @@ using AutoMapper;
 using AuthDatabase.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ClientWebApp.Controllers
 {
     public class LibraryItemController : Controller
     {
         private readonly ILibraryItemService _libraryItemService;
-        //private readonly UserManager<AppUser> _userManager;
+        private readonly UserManager<AppUser> _userManager;
 
 
-        public LibraryItemController(ILibraryItemService libraryItemService
-            //, UserManager<AppUser> userManager
-            )
+        public LibraryItemController(ILibraryItemService libraryItemService, UserManager<AppUser> userManager)
         {
             _libraryItemService = libraryItemService;
-            //_userManager = userManager;
+            _userManager = userManager;
 
         }
 
@@ -41,7 +40,40 @@ namespace ClientWebApp.Controllers
             return View(model);
         }
 
-        public async Task<IActionResult> AddLibraryItem(LibraryItemViewModel newItem)
+        //public ViewResult EnterData()//(UserManager<AppUser> userManager)
+        public async Task<IActionResult> EnterData()
+        {
+            LibraryItemViewModel[] libraryItemsList = await _libraryItemService.GetAll();
+            var model = new LibraryItemViewModel();
+            List<Author> list = new List<Author>();
+            //IEnumerable<SelectListItem>;
+            foreach (var a in libraryItemsList) 
+            {
+                if (!list.Contains(a.Author))
+                {
+                    list.Add(a.Author);
+                }
+            }
+            // = libraryItemsList
+            ViewBag.AuthorsViewBag = list;
+            return View(model);
+        }
+
+
+
+        //public async Task<IActionResult> SaveLibraryItem(LibraryItemViewModel newItem)
+        //{
+        //    int successfullTran = await _libraryItemService.AddLibraryItem(newItem);
+        //    if (successfullTran > 0)
+        //    {
+        //        return BadRequest("Could not add item.");
+        //    }
+
+        //    return RedirectToAction("LibraryItemsList");
+
+        //}
+
+        public async Task<IActionResult> SaveLibraryItem(LibraryItemViewModel newItem)
         {
             int successfullTran = await _libraryItemService.AddLibraryItem(newItem);
             if (successfullTran > 0)
